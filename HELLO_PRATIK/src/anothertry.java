5ab5661deed2d378860076b00b013dc81fcf1022
pratikgole <pratikgole5682@gmail.com>
2020-08-09 19:09:42 +0530

    version2

--- HELLO_PRATIK/src/anothertry.java
+++ HELLO_PRATIK/src/anothertry.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class anothertry {
 		public static void main (String[]args){
+			System.out.println("thhis is updated");
 		System.out.println("Enter the numbers");
 		Scanner input = new Scanner(System.in);
 		int num1 = input.nextInt();
