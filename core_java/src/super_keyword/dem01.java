0bea7277447aaf7c0d3a3193313d69e20647ba19
pratikgole <pratikgole5682@gmail.com>
2020-08-09 19:54:20 +0530

    demo1.1

---
+++ core_java/src/super_keyword/dem01.java
@@ -0,0 +1,45 @@
+package super_keyword;
+
+import operators.demo1;
+
+//super keyword is used if wewant to get anything from parent class,so basically it works with inheritance
+//we can call instance variables,or methods and constructors,
+//in following program  we will use to call everything from bike
+
+class bike{
+	int no;
+	String company;
+	bike(int i,String c)
+	{no=i;
+	company=c;
+		
+	}
+	
+}
+class model extends bike{
+	String type;
+	
+	model(int no, String company,String m)
+	{ super(no,company);
+	type=m;
+    }
+	
+	
+	void show()
+	{
+		System.out.println("bike number="+no+"\nbike company="+company+"\nmodel="+type);
+	}
+}
+public class dem01 {
+
+     public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      //demo1 dd=new demo1(1202,"dominar","bajaj");
+     model mm=new model(1202,"bajaj","dominar");
+     mm.show();
+      
+		
+		
+	}
+
+}
